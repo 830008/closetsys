07a264d6e0759ea2e4b10bf78535b97fec38915e
830008 <nrollins189@gmail.com>
2024-02-18 16:39:37 -0600

    Add web ui

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const johnnyFive = require('johnny-five');
+const express = require('express');
 const board = new johnnyFive.Board(4);
 board.on('ready', () => {
-    const relay1 = new johnnyFive.Relay(1);
     const relay2 = new johnnyFive.Relay(2);
     const relay3 = new johnnyFive.Relay(3);
     const relay4 = new johnnyFive.Relay(4);
-    relay1.open();
-    relay2.open();
-    relay3.open();
-    relay4.open();
+    const relay5 = new johnnyFive.Relay(5);
+    const app = express();
+    app.use(express.static('static'));
+    app.post('/relay/:id/on', (req, res) => {
+        const id = req.params.id;
+        const relay = new johnnyFive.Relay(id);
+        relay.close();
+        res.send('Relay ' + id + ' is on');
+    });
+    app.post('/relay/:id/off', (req, res) => {
+        const id = req.params.id;
+        const relay = new johnnyFive.Relay(id);
+        relay.open();
+        res.send('Relay ' + id + ' is off');
+    });
+
+    app.listen(3000, () => {
+        console.log('Server started on http://localhost:3000');
+    });
+
 });
